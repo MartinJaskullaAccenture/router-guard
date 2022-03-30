7e72bf243f109dbed623333d6de752037cb8af3e
Martin Jaskulla <martinjaskulla@icloud.com>
2022-03-30 14:55:48 +0200

    Replace <a> with router.push button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,20 @@
 import type { NextPage } from 'next'
 import Link from "next/link";
+import { useRouter } from 'next/router';
 
 const Home: NextPage = () => {
-  return <>
-    <h1>Home</h1>
-    <ul>
-      <li><Link href="/sales">Sales</Link></li>
-      <li><Link href="/mein-sky">Mein Sky</Link></li>
-      <li><a href="https://google.com">Google</a></li>
-      <li><a href="https://google.com" target="_blank">Google New Tab</a></li>
-    </ul>
-  </>
+    const router = useRouter()
+
+    return <>
+        <h1>Home</h1>
+        <ul>
+            <li><Link href="/sales">Sales</Link></li>
+            <li><Link href="/mein-sky">Mein Sky</Link></li>
+            <li>
+                <button onClick={() => router.push("/sales")}>router.push(/sales)</button>
+            </li>
+        </ul>
+    </>
 }
 
 export default Home
